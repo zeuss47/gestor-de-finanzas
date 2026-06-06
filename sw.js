f0a933a840654c9b7892b511fe0757857ce7cc21
zeuss47 <axelfrankowski@gmail.com>
2026-06-06 02:35:57 -0300

    v2.1 build#118 Â· M .gitignore, D _pp3/Default/Cache/Cache_Data/data_0, D _...

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  *  4. MESSAGE SKIP_WAITING: el cliente decide cuando activar el SW nuevo.
  */
 
-const VERSION = 'v3.3.0-pages-b117';
+const VERSION = 'v3.3.0-pages-b118';
 const SHELL_CACHE = `shell-${VERSION}`;
 
 // Base scope dinamico: el SW se registra en su propio directorio,
